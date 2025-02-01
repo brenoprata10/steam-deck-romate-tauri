e280fc0da8e19ca36c6c032a51e727643ffeddd9
Breno Prata <brenoprata@hotmail.com>
2025-02-01 22:35:58 +0100

    Add comment

--- src/hooks/useSelectFolder.tsx
+++ src/hooks/useSelectFolder.tsx
@@ -3,6 +3,7 @@ import {TFolderPath} from '@/types/TFilePath'
 
 const useSelectFolder = (title: string): {trigger: () => Promise<TFolderPath>} => {
 	//TODO migrate to Command
+	// use Dialog plugin here
 	//return {trigger: () => Electron.ipcRenderer.invoke(EChannel.SELECT_FOLDER, title)}
 	return {trigger: () => Promise.resolve({canceled: false, filePaths: []})}
 }
