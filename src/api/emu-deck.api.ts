3c9f2d10f09b1bee16f3b85050431b61f7d21cbc
Breno Prata <brenoprata@hotmail.com>
2025-02-12 20:26:29 +0100

    Add steam deck romate file from emudeck

--- src/api/emu-deck.api.ts
+++ src/api/emu-deck.api.ts
@@ -1,22 +1,20 @@
 import TParserConfig from '@/types/TParserConfig'
-import TSteamRomManagerParserConfig from '@/types/TSteamRomManagerParserConfig'
 import {fetch} from '@tauri-apps/plugin-http'
 
 export const getEmuDeckConfigFile = async (emulationFolderPath: string): Promise<TParserConfig[]> => {
 	try {
 		const response = await fetch(
-			'https://raw.githubusercontent.com/dragoonDorise/EmuDeck/main/configs/steam-rom-manager/userData/userConfigurations.json'
+			'https://raw.githubusercontent.com/dragoonDorise/EmuDeck/refs/heads/main/configs/steam-deck-romate/userConfigurations.json'
 		)
 
-		const emuDeckParserFile = (await response.json()) as TSteamRomManagerParserConfig[]
+		const emuDeckParserFile = (await response.json()) as TParserConfig[]
 		console.log(emuDeckParserFile)
 		return emuDeckParserFile.map((parser) => ({
 			...parser,
-			id: parser.configTitle,
-			name: parser.configTitle,
-			supportedFileTypes:
-				parser.parserInputs.glob?.match(/\.\w+/g)?.map((match) => match.toString().replace('.', '')) ?? [],
-			romDirectory: `${emulationFolderPath}/${parser.romDirectory.replace('/run/media/mmcblk0p1/', '').replace('${romsdirglobal}', 'roms')}`
+			id: parser.id,
+			name: parser.name,
+			supportedFileTypes: parser.supportedFileTypes,
+			romDirectory: `${emulationFolderPath}/${parser.romDirectory}`
 		}))
 	} catch (error) {
 		console.error(error)
